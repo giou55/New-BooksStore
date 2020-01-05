8ae4210ff1cca19dc33360187f17479fc43fa1a6
Giorgos Giourmetakis <giorgosggg@gmail.com>
2020-01-05 18:18:41 +0200

    changes

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -5,7 +5,6 @@ import { FormsModule } from "@angular/forms";
 import { ModelModule } from "../model/model.module";
 import { StoreComponent } from "./store.component";
 import { CounterDirective } from "./counter.directive";
-import { TopbarComponent } from "./topbar/topbar.component";
 import { FooterComponent } from './footer/footer.component';
 import { CartSummaryComponent } from "./cart/cartSummary.component";
 import { CartDetailComponent } from "./cart/cartDetail.component";
@@ -16,7 +15,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
     imports: [ModelModule, CommonModule, BrowserModule, FormsModule, RouterModule, 
               BrowserAnimationsModule],
-    declarations: [StoreComponent, CounterDirective, TopbarComponent, CartSummaryComponent,
+    declarations: [StoreComponent, CounterDirective, CartSummaryComponent,
                    CartDetailComponent, CheckoutComponent, FooterComponent],
     exports: [StoreComponent, CartDetailComponent, CheckoutComponent]
 })
